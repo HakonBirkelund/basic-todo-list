a662ae2134b79735827e408b30d335558608fd8b
Håkon Kristian Birkelund <99222887+L0WC0ST-GR0VE@users.noreply.github.com>
2022-04-02 02:57:41 +0200

    Added most of the functionality
    Fully implemented:
    - Add todo function (add to todo list)
    - Show todo function (display the list to the user, in a "presentable way"
    Not yet implemented:
    - Delete item function (remove an item from the todo list)

--- todo.py
+++ todo.py
@@ -3,22 +3,25 @@ def add_todo():
     while Done == False:
         entry = input("What would you like to add to the todo list?")
         lst.append(entry)
-        completed = input("Are you done adding new entries?\nY/N ->")
+        completed = input("Are you done adding new entries?\nY/N -> ")
         if completed == "Y":
             Done = True
     with open("todo.txt", "a") as f:
         for x in lst:
             f.write(x + '\n')
 
-def delete_todo(lst:list):
-    with open("todo.txt", "w") as f:
-        for x in lst:
-            f.write(x)
+#def delete_todo(lst:list):
+    #with open("todo.txt", "w") as f:
+    #    for x in lst:
+    #        f.write(x)
+    # TODO Add option to delete an item from the list.
 
 def show_todo():
     print("Your todo list:")
     with open("todo.txt", "r") as f:
-        print(f.readlines())
+        output = f.read().splitlines()
+        for i in output:
+            print("-" + i)
 
 
 def main():
@@ -26,17 +29,24 @@ def main():
     while Done == False:
         print("What would you like to do?\
             \nSee todo list -> A\
-            \nAdd an item to the todo list -> B\
+            \nAdd items to the todo list -> B\
             \nRemove an item from the todo list -> C\
             \nClose todo list -> D")
         choice = input("-> ")
-        if choice not in "ABC":
-            print("Invalid option. Please select another.")
+        if choice not in "ABCD":
+            print("Invalid option. Please select another.\
+                \n-------------------------------------")
         else:
             if choice == "A":
                 show_todo()
             elif choice == "B":
                 add_todo()
-            elif choice == "C":
-                delete_todo()
+            #elif choice == "C":
+            # TODO   delete_todo()
+            elif choice == "D":
+                print("Thank you for using todo list.\
+                    \nI'm proud of your accomplishments!")
+                
+                Done = True
+                return Done
 main()    
