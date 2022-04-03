2c56aa249fd2b6c703ca29b217a5362548880bea
Håkon Kristian Birkelund <99222887+HakonBirkelund@users.noreply.github.com>
2022-04-03 04:26:55 +0200

    Large overhaul!
    Most of the program got a complete overhaul. Here's a list of changes:
    
    - A lot of bug-proofing was added, preventing improper input and errors breaking the program.
    
    - Changed data storage to a .JSON file instead of a .txt file. With almost no experience with JSON prior, this took way too long to do.
    
    What remains to be finished is fixing the bugs from the delete function, and writing in some cleaner printouts for the user.

--- todo.py
+++ todo.py
@@ -1,52 +1,67 @@
+import json
+
 def add_todo():
-    lst, Done = [], False
-    while Done == False:
-        entry = input("What would you like to add to the todo list?")
-        lst.append(entry)
-        completed = input("Are you done adding new entries?\nY/N -> ")
-        if completed == "Y":
-            Done = True
-    with open("todo.txt", "a") as f:
-        for x in lst:
-            f.write(x + '\n')
-
-#def delete_todo(lst:list):
-    #with open("todo.txt", "w") as f:
-    #    for x in lst:
-    #        f.write(x)
-    # TODO Add option to delete an item from the list.
-
+    entry = input("What would you like to add to the todo list?\n-> ")
+    with open("todo.json", 'r') as json_file:
+        try:
+            data = json.load(json_file)
+            todo_name = len(data)
+        except:
+            todo_name = 0
+    data[f'{todo_name+1}'] = entry
+    with open("todo.json", "w") as jsonFile:
+        json.dump(data, jsonFile)
+        print("Successfully added todo item!")
+        
+def delete_todo():
+    del_line = "."
+    with open("todo.json", 'r') as json_file:
+        data = json.load(json_file)
+    if data != {}:
+        print("Todo list items:")
+        for i in data:
+            print(f'{data[i]} - {i}')
+        while del_line.isnumeric() == False:
+            del_line = input("Which entry do you want to delete ?\n-> ")
+            try:
+                if del_line in data[del_line]:
+                    del data[del_line]
+            except:
+                print("There is no such entry!")
+        with open('todo.json', 'w') as json_file:
+            json.dump(data, json_file)
+            print("Successfully deleted the todo item!")
+    else:
+        print("No items to delete!")
+    
 def show_todo():
-    print("Your todo list:")
-    with open("todo.txt", "r") as f:
-        output = f.read().splitlines()
-        for i in output:
-            print("-" + i)
-
-
-def main():
+    with open("todo.json") as f:
+        items = json.load(f)
+        if items:
+            print("Your todo list:")
+            for i in items:
+                print(f'{items[i]}')
+        else:
+            print("No items on the todo list!")
+        
+def todo():
     Done = False
     while Done == False:
-        print("What would you like to do?\
-            \nSee todo list -> A\
-            \nAdd items to the todo list -> B\
-            \nRemove an item from the todo list -> C\
-            \nClose todo list -> D")
+        print("What would you like to do?\nSee todo list -> A\nAdd items to the todo list -> B\
+            \nRemove an item from the todo list -> C\nClose todo list -> D")
         choice = input("-> ")
-        if choice not in "ABCD":
+        if choice not in "AaBbCcDd":
             print("Invalid option. Please select another.\
                 \n-------------------------------------")
         else:
-            if choice == "A":
+            if choice in "Aa":
                 show_todo()
-            elif choice == "B":
+            elif choice in "Bb":
                 add_todo()
-            #elif choice == "C":
-            # TODO   delete_todo()
-            elif choice == "D":
+            elif choice in "Cc":
+                delete_todo()
+            elif choice in "Dd":
                 print("Thank you for using todo list.\
                     \nI'm proud of your accomplishments!")
-                
                 Done = True
-                return Done
-main()    
+todo()    
