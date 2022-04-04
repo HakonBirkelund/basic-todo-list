0d343d490ed7e9bcb163c151157abefa20294d63
Håkon Kristian Birkelund <99222887+HakonBirkelund@users.noreply.github.com>
2022-04-05 01:19:30 +0200

    Almost full release - minor adjustments soon!
    Finished, took some time due to other studies having priority.
    
    Changes:
    [+] Changed storage method to list, from dictionary
    [+] Implemented simple auxiliary functions separator and read_json. The former simply prints a line to separate sections of the menu in the terminal.
    [+] Rewrote the delete_todo function, with a lot of care put into handling all types of improper input.
    [+] Included a lot of in-code documentation as comments. I'm not used to commenting my code in a specific way, but I like to have neat blocks of comments when I can.
    [?] Considering making it so that in the future, the add_todo function has a character limit, to prevent overflooding text in the printouts.

--- todo.py
+++ todo.py
@@ -1,70 +1,108 @@
 import json
 
-def add_todo():
-    entry = input("What would you like to add to the todo list?\n-> ")
+
+def separator() -> str:
+    """Prints a string of '-' to separate parts of the terminal.
+
+    Returns:
+        str: a line made of '-'s
+    """
+    print("------------------------------------------")
+
+
+def read_json() -> list:
+    """Reads todo.json
+
+    Returns:
+        list: A list of all to do tasks.
+    """
     with open("todo.json", 'r') as json_file:
         try:
-            data = json.load(json_file)
-            # having a mental breakdown trying to maintain the dictionary when adding a new entry.
+            return json.load(json_file)
         except:
-            todo_name = 1
-    data[f'{todo_name+1}'] = entry
-    with open("todo.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
-        print("Successfully added todo item!")
-        
-def delete_todo():
-    del_line = "."
-    with open("todo.json", 'r') as json_read:
-        data = json.load(json_read)
-    if data != {}:
-        print("Todo list items:")
-        for i in data:
-            print(f'{data[i]} - {i}')      
-    else: return -1  
-    while del_line.isnumeric() == False:
-        del_line = input("Which entry do you want to delete ?\n-> ")
-    try:
-        if del_line in data:
-            data.pop(data[del_line])
-        print(data)
-    except:
-        print("There is no such entry!")
-        return -1
-    
-    with open('todo.json', 'w') as json_write:
-        json.dump(data, json_write)
-        print("Successfully deleted the todo item!")
+            print("There was an error reading the file.")
+# General functions to improve readability of the code.
 
-    
+
+def add_todo():
+    """Adds a task to the to do list.
+    """
+    entry = input("What would you like to add to the to do list?\n-> ")
+    data = read_json()  # Access the file and save as data   |
+    data.append(entry)  # Append the new entry to the list   |
+    with open("todo.json", "w") as jsonFile:  # Open the JSON|
+        json.dump(data, jsonFile)             # Add new task.|
+        print("Successfully added new task!")  # Print out. |
+    separator()
+
+
+def delete_todo():
+    """Deletes a task from the todo list.
+
+    Returns:
+        bool: Returns a False value if the deletion was unsuccessful.
+    """
+    del_line, data = ".", read_json()       # default del_line, get data \\\
+    if data != []:                          # If list isn't empty, print  \\\
+        for counter, i in enumerate(data):  # the tasks in it. E.g:        \|
+            print(f'{counter} - {i}')       # 0 - Water the plants upstairs |
+        while not del_line.isnumeric():     # 1 - Study Python Programming  |
+            del_line = input("Which entry do you want to delete?\
+                \n(enter a positive, non-existant number to cancel)\n-> ")
+
+        del_line = int(del_line)     # cast the input, meaning no try/except|
+        if del_line >= 0 and del_line <= len(data)-1:   # If input is valid |
+            data.pop(del_line)        # remove the task the user specified  |
+            with open('todo.json', 'w') as json_write:        # Update JSON |
+                json.dump(data, json_write)                   # Also, super |
+                separator()                                   # cool prints |
+                print("Successfully deleted the todo item!")
+                separator()
+        else:                    # If the input is outside list index range |
+            separator()          # Run this block instead and return False  |
+            print("There is no such entry, deletion cancelled.")
+            separator()
+            return False                                  # If all of the   |
+    else:                                                 # fails, the todo |
+        print("There are no entries in the todo list!")   # list is empty.. |
+        return False                                      # Return False    |
+
+
 def show_todo():
-    with open("todo.json") as f:
-        items = json.load(f)
-        if items:
-            print("Your todo list:")
-            for i in items:
-                print(f'{items[i]}')
-        else:
-            print("No items on the todo list!")
-        
+    data = read_json()         # Access the file and save as data \
+    if data:                   # If the to do has at least one task\
+        print("Your to do list:")  # the whole list will be printed/ 
+        for i in data:         # & formatted like this for users: /
+            print(f'- {i}')    # - Water the plants upstairs    /
+    else:                      # - Study Python programming    /
+        print("No tasks on the to do list!")  # <- Print this /
+    separator()                # if there weren't any tasks. /
+
+
 def todo():
-    Done = False
-    while Done == False:
-        print("What would you like to do?\nSee todo list -> A\nAdd items to the todo list -> B\
-            \nRemove an item from the todo list -> C\nClose todo list -> D")
-        choice = input("-> ")
-        if choice not in "AaBbCcDd":
-            print("Invalid option. Please select another.\
-                \n-------------------------------------")
+    """Starts the to do list program.
+    """
+    Done = False     # The program will run until |
+    while not Done:  # the user ends the program. |
+        print("What would you like to do?\
+            \nSee to do list -> A\
+            \nAdd items to the to do list -> B\
+            \nRemove a task from the to do list -> C\
+            \nClose to do list -> D")  # D/d will quit it\
+        choice = input("-> ")         # Input the choice.|
+        if choice not in "AaBbCcDd":  # Check validity. /
+            print("Invalid option. Please select another.")
+            separator()
         else:
-            if choice in "Aa":
-                show_todo()
-            elif choice in "Bb":
-                add_todo()
-            elif choice in "Cc":
-                delete_todo()
-            elif choice in "Dd":
-                print("Thank you for using todo list.\
+            separator()
+            if choice in "Aa":    # This program does not need returns from \
+                show_todo()       # the auxiliary functions in order to work.|
+            elif choice in "Bb":  # Functions show_todo() & add_todo() don't |
+                add_todo()        # have those, but you could implement it if|
+            elif choice in "Cc":  # desired. Unlike those two, delete_todo() |
+                delete_todo()     # can return False, which can be useful for|
+            elif choice in "Dd":  # cases where you may have to find a bug. /
+                print("Thank you for using the to do list.\
                     \nI'm proud of your accomplishments!")
-                Done = True
-todo()    
+                Done = True       # Program is done, change Done to True.
+todo()
